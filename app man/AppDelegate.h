a1e3705e6257e0462d86d380dd4263cca0a6f4b5
apple <apple@appledeMacBook-Pro.local>
2015-04-28 21:18:36 +0800

    Initial Commit

---
+++ app man/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  app man
+//
+//  Created by apple on 15/4/28.
+//  Copyright (c) 2015年 worldelite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ app man/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  app man
+//
+//  Created by apple on 15/4/28.
+//  Copyright (c) 2015年 worldelite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
